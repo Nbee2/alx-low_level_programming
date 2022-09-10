cd3b23d276fe99ce64aedcddb30c8b53b5712b3e
Jibril Isah <jibrilisah1@gmail.com>
2022-09-09 23:47:28 -0700

    Update task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,15 @@ int main(void)
 	
 	for  (i = 0; i < 100; i++)
 	{
-		for (p = 0; p < 100; p++)
+		for (q = 0; q < 100; q++)
 		{
 			if (p > i)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
+				putchar((q / 10) + '0');
+				putchar((q % 10) + '0');
 				if (i != 98)
 				{
 					putchar(',');
