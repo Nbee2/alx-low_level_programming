bd99cf35ea72c44dddfa59b06758c1ebc07df9a0
Jibril Isah <jibrilisah1@gmail.com>
2022-09-09 22:35:26 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - print possible combo of 3
+*
+* Return: returns zero at the end
+*/
+int main(void)
+{
+int n, p, m;
+
+for (n = 0; n <= 9; n++)
+	{
+	for (p = 1; p <= 9; p++)
+		{
+		for (m = 2; m <= 9; m++)
+			{
+			if (m > p && p > n)
+				{
+					putchar(n + '0');
+					purchar(p + '0');
+					purchar(m + '0');
+						if (n != 7)
+						{
+						putchar(',');
+						putchar(' ');
+						}
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
