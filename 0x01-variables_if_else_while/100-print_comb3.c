eec29040f32e508fd1e9ecaeb5ad6f70b314b539
Jibril Isah <jibrilisah1@gmail.com>
2022-09-09 22:00:58 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - printsingle digit numbers starting from 0
+*
+* Return: zero at the end
+*/
+int main(void)
+{
+int n, m;
+
+for (n = 0; i <= 9; i++)
+{
+	for (m = 1; m <=9; m++)
+	{
+		if (m > n)
+		{
+			putchar(n +'0');
+			putchar(m + '0');
+			if (n != 8)
+			{
+				putchar(',');
+				putchar('');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
