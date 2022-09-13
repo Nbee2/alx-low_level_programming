8756398a4d50da288168090d9c7cb9bcad2ff2c1
Jibril Isah <jibrilisah1@gmail.com>
2022-09-13 16:48:07 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+* main - a program that prints _putchar,
+* followed by a new line.
+*
+*Get the character to be written.
+* Write the Character to stdout.
+* Return: always
+*/
+int main(voi  
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	
+	return (0);
+}
