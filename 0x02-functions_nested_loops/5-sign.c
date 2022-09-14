abcdae240af70c8c4ae78968267d2bd626c52cdc
Jibril Isah <jibrilisah1@gmail.com>
2022-09-13 23:49:53 -0700

    task 5 Updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
 * print_sign - function that prints sign
 * of a number.
-* n: is the int that will be use for the
+* @n: is the int that will be use for the
 * argument of the function.
 * Return: 0
 */
