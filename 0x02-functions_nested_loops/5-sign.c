a9db0e3a77ae90467f8e277941eb632e6f0112bc
Jibril Isah <jibrilisah1@gmail.com>
2022-09-13 23:46:29 -0700

    Task 5 Updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* function that prints sign of a number.
+* print_sign - function that prints sign
+* of a number.
 * n: is the int that will be use for the
 * argument of the function.
 * Return: 0
@@ -8,7 +9,7 @@
 
 int print_sign(int n)
 {
-	if (n > 0) 
+	if (n > 0)
 	{
 	_putchar('+');
 	return (1);
@@ -21,6 +22,6 @@ int print_sign(int n)
 	else
 	{
 	_putchar('0');
-	return (0);	
+	return (0);
 	}
 }
