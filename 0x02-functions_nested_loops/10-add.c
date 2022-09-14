b68241c2b3430d2785c3fecaf30e73be255676a2
Jibril Isah <jibrilisah1@gmail.com>
2022-09-14 04:33:29 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,10 @@
 * add - function that write the sum of
 * two integer.
 *
-* @num1: first integer to be added
-* @num2: second integer to be added
-* 
+* @num1: first integer to be added.
+* @num2: second integer to be added.
 * Return: the result addition
 */
-
 int add(int num1, int num2)
 {
 		return (num1 + num2);
