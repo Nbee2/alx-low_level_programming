045eb2bce786f336a61de446dca9dbc19b2bfae0
Jibril Isah <jibrilisah1@gmail.com>
2022-09-19 09:29:57 -0700

    Task 5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+/**
+* print_rev - Write a function that reverses a string.
+* @s: The string tp print
+* Return: Void
+*/
+void rev_string(char *s)
+{
+	int k = 0:
+
+	while (s[k] != '\0')
+	{
+		k++;
+	}
+
+	for (k -= 1; c >= 0; k--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,5 +17,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
-
+int _putchar(chat);
 #endif /*MAIN_H*/
