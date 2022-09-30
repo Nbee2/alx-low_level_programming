f29d0ec65d49aa435b43a5cb1686fb122f0163f2
Jibril Isah <jibrilisah1@gmail.com>
2022-09-29 19:03:40 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 
+
 int check_pal(char *s, int i, int len);
 
 int _strlen_recursion(char *s);
@@ -9,25 +10,25 @@ int _strlen_recursion(char *s);
 
 /**
  *
- * is_palindrome - checks if a string is a palindrome
+ *  * is_palindrome - checks if a string is a palindrome
  *
- * @s: string to reverse
+ *   * @s: string to reverse
  *
+ *    *
  *
+ *     * Return: 1 if it is, 0 it's not
  *
- * Return: 1 if it is, 0 it's not
- *
- */
+ *      */
 
 int is_palindrome(char *s)
 
 {
 
-		 if (*s == 0)
+	 if (*s == 0)
 
-		 return (1);
+		   return (1);
 
-		 return (check_pal(s, 0, _strlen_recursion(s)));
+	  return (check_pal(s, 0, _strlen_recursion(s)));
 
 }
 
@@ -35,15 +36,15 @@ int is_palindrome(char *s)
 
 /**
  *
- * _strlen_recursion - returns the length of a string
+ *  * _strlen_recursion - returns the length of a string
  *
- * @s: string to calculate the length of
+ *   * @s: string to calculate the length of
  *
+ *    *
  *
+ *     * Return: length of the string
  *
- * Return: length of the string
- *
- */
+ *      */
 
 int _strlen_recursion(char *s)
 
@@ -51,9 +52,9 @@ int _strlen_recursion(char *s)
 
 	 if (*s == '\0')
 
-		 return (0);
+		   return (0);
 
-	 return (1 + _strlen_recursion(s + 1));
+	  return (1 + _strlen_recursion(s + 1));
 
 }
 
@@ -61,19 +62,19 @@ int _strlen_recursion(char *s)
 
 /**
  *
- * check_pal - checks the characters recursively for palindrome
+ *  * check_pal - checks the characters recursively for palindrome
  *
- * @s: string to check
+ *   * @s: string to check
  *
- * @i: iterator
+ *    * @i: iterator
  *
- * @len: length of the string
+ *     * @len: length of the string
  *
+ *      *
  *
+ *       * Return: 1 if palindrome, 0 if not
  *
- * Return: 1 if palindrome, 0 if not
- *
- */
+ *        */
 
 int check_pal(char *s, int i, int len)
 
@@ -81,12 +82,12 @@ int check_pal(char *s, int i, int len)
 
 	 if (*(s + i) != *(s + len - 1))
 
-		 return (0);
+		   return (0);
 
-	 if (i >= len)
+	  if (i >= len)
 
-		 return (1);
+		    return (1);
 
-	 return (check_pal(s, i + 1, len - 1));
+	   return (check_pal(s, i + 1, len - 1));
 
 }
