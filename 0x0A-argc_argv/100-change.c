108a8d892ae4512b39d45ee8255fa569631b5b72
Jibril Isah <jibrilisah1@gmail.com>
2022-10-01 16:04:43 -0700

    Task 5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,6 @@
 */
 int main(int argc, char *argv[])
 {
-
 	int cents, coins = 0;
 
 	if (argc != 2)
@@ -19,35 +18,28 @@ int main(int argc, char *argv[])
 
 	printf("Error\n");
 	return (1);
-
 	}	
-
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 
 	{
 
 	coins++;
-
 	if ((cents - 25) >= 0)
 	{
 		cents -= 25;
 		continue;
 	}
-
 	if ((cents - 10) >= 0)
 	{
 		cents -= 10;
 		continue;
 	}
-
 	if ((cents - 5) >= 0)
 	{
 		cents -= 5;
 		continue;
 	}
-
 	if ((cents - 2) >= 0)
 	{
 		cents -= 2;
