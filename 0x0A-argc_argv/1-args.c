17b92156c598b606fab6924a4926f31df3275f2a
Jibril Isah <jibrilisah1@gmail.com>
2022-10-01 14:54:01 -0700

    Task 1. Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - prints the name number of arguments passed to it
  *
- *  * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
  *
- *   * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- *    * @argv: argument vector of pointers to strings
- *
- *     *
- *
- *      * Return: always 0
- *
- *       */
+ * Return: always 0
+ */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 
@@ -22,6 +16,6 @@ int main(int argc, char *argv[] __attribute__((unused)))
 
 		printf("%d\n", argc - 1);
 
-			return (0);
+		return (0);
 
 }
