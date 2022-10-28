54d064824a0819bd53808494c2f7c735f23c22f3
Jibril Isah <jibrilisah1@gmail.com>
2022-10-28 15:38:34 -0700

    Task 4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdlib.h>
 /**
- * clear_bit - Entry Point
- * @n: input
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: parameter
  * @index: index
- * Return: 0
+ * Return: 1 if works, -1 if error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > sizeof(n) * 8)
 		return (-1);
-
-	*n = *n & ~(1ul << index);
+	*n &= ~(1 << index);
 	return (1);
 }
